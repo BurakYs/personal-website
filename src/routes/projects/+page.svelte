2e06a25c4df4d7ae57263327dd9472808c7d27a9
BurakYs <brkyhs0@gmail.com>
2024-11-16 01:39:13 +0300

    .

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -5,7 +5,7 @@
 <div class="mt-10 mb-10 p-4">
   <h1 class="text-3xl font-bold text-white">Projects</h1>
   <div class="flex justify-center">
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-5">
       {#each config.projects as project}
         <a href={project.url || "?"}>
           <div
