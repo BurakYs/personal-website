58987c027705be92e7d1016fa22ec033f4041b03
BurakYs <brkyhs0@gmail.com>
2025-01-04 21:31:44 +0300

    Remove unnecessary meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import '$lib/styles/globals.css';
 
-  import { page } from '$app/stores';
   import Header from '$lib/components/Header.svelte';
   import Footer from '$lib/components/Footer.svelte';
   import config from '$lib/config';
@@ -13,9 +12,7 @@
   <title>{config.meta.title}</title>
   <meta name="og:title" content={config.meta.title}/>
   <meta name="og:description" content={config.meta.description}/>
-  <meta name="og:url" content={$page.url.origin}/>
   <meta name="twitter:description" content={config.meta.description}/>
-  <meta name="twitter:url" content={$page.url.origin}/>
   <meta name="twitter:card" content="summary_large_image"/>
   <link rel="icon" href={config.meta.favicon}>
   <link rel="preconnect" href="https://fonts.googleapis.com"/>
